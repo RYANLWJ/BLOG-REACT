36e12c3f6ebbd148ae8a57cab7387cb8b89b50cd
RYAN LI <essentialmomo@icloud.com>
2020-01-14 16:26:15 +0800

    增加了header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 
-
 export default class Header extends Component {
   constructor(props) {
-    super(props)
-    this.state={
-        bool:true,
-    }
-};
+    super(props);
+    this.state = {
+      bool: true
+    };
+  }
 
   render() {
     return <h1>111222</h1>;
   }
-
- 
-
-
 }
-
-
-
-
